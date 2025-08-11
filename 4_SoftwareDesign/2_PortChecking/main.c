a44c3fa1b2b534880a02dc038329698584d1abcf
Lalji J <mrlalji.joshi@gmail.com>
2025-08-11 05:42:54 +0530

    Validate ports
    - Validate all port using blinkLED one by one

---
+++ 4_SoftwareDesign/2_PortChecking/main.c
@@ -0,0 +1,80 @@
+// All standard header file declaration
+#include <reg51.h>
+
+// All user define header file declaration
+#include "main.h"
+#include "timer.h"
+#include "led.h"
+
+// Macro define
+
+
+//Global variables
+
+
+
+// Start main function
+void main(void)
+{
+    init_all_port();
+
+    blink_port0_leds();
+		// blink_port1_leds();
+		// blink_port2_leds();
+		// blink_port3_leds();
+
+}
+
+
+
+
+// Functions declration
+/* 
+Initialize all ports for output
+If we write on 1 on this port then it will on the LED.
+*/
+void init_all_port(void)
+{
+    init_port0();
+    init_port1();
+    init_port2();
+    init_port3();
+}
+
+/* 
+Initialize port0 for output
+If we write on 1 on this port then it will on the LED.
+*/
+void init_port0(void)
+{
+    P0 = 0x00;
+}
+
+/*
+Initialize port1 for output
+If we write on 1 on this port then it will on the LED.
+*/
+void init_port1(void)
+{
+    P1 = 0x00;
+}
+
+
+/*
+Initialize port2 for output
+If we write on 1 on this port then it will on the LED.
+*/
+void init_port2(void)
+{
+    P2 = 0x00;
+}
+
+
+/*
+Initialize port3 for output
+If we write on 1 on this port then it will on the LED.
+*/
+void init_port3(void)
+{
+    P3 = 0x00;
+}
